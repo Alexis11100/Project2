068e56cf9961a6580bc40d27a7b1f57a06ca5ef0
coole <alexisbentley564@gmail.com>
2025-12-03 20:34:18 -0600

    Adding files

---
+++ main.py
@@ -0,0 +1,15 @@
+
+from PyQt6.QtWidgets import QApplication
+from logic import Logic
+from gui import *
+
+def main():
+    application = QApplication([])
+    window = Logic()
+    window.show()
+    application.exec()
+
+if __name__ == "__main__":
+    main()
+
+
